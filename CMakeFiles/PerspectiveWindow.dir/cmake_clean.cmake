7b5a759479cd81a2303f0a244b20fea04f0f4037
Alexander Mai <amai@Alexanders-MacBook-Pro.local>
2015-12-24 19:08:56 -0800

    project setup

---
+++ CMakeFiles/PerspectiveWindow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PerspectiveWindow.dir/main.cpp.o"
+  "PerspectiveWindow.pdb"
+  "PerspectiveWindow"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PerspectiveWindow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+int startingHeight( int winSize[], float camDirect[] );
+int * windowSizeAt( int winSize[], int pos[], float camDirect[] );
+int * windowSizeForIndex( int index, int winSize[], float camDirect[] );
+float diffMagnitude( Mat m1, Mat m2 );
+
+int main ( int argc, char** args )
+{
+	if (argc != 5) 
+	{
+		cout << "Usage: PerspectiveWindow <Image> <X> <Y> <Z>, where x y z are camera perspective" << endl;
+	}
+	cout << * windowSizeAt((int[]){300, 300}, (int[]){100, 100}, (float[]){0.0, 0.0, 1.0}) << endl;
+	cout << "Hello World" << endl;
+	
+}
+
+int * windowSizeAt( int winSize[], int pos[], float camDirect[] )
+{
+	static int p[] = {5, 4, 3};
+	return p;
+}
